95f8eb775e6f1852512a5d2241c21a89d5a5f971
Tarbooza <hassaan.butt9999@outlook.com>
2019-11-24 22:37:34 +0500

    Document Fornatting commit

--- binary_tree.cpp
+++ binary_tree.cpp
@@ -1,57 +1,58 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-struct node{
+struct node
+{
 	int data;
-	node* left;
-	node* right;
+	node *left;
+	node *right;
 };
 
-class B_Tree{
-	private:
-		 node* root;
-		 void destroy();
-		 void insert(node*root, int data);
-		 node* search(node* root,int data);
-		 void del(node* root,int data);
-		 void inorder(node *root);
-		 void postorder(node *root );
-		 void preorder(node *root);
-	public:
-		void destroy()
-			{
+class B_Tree
+{
+private:
+	node *root;
+	void destroy();
+	void insert(node *root, int data);
+	node *search(node *root, int data);
+	void del(node *root, int data);
+	void inorder(node *root);
+	void postorder(node *root);
+	void preorder(node *root);
 
-			}
-		void insert(node* root,int data)
+public:
+	void destroy()
+	{
+	}
+	void insert(node *root, int data)
+	{
+		if (data < root->data)
+		{
+			if (root->left != NULL)
 			{
-			if(data < root->data)
-				{
-				if(root->left != NULL)
-                {
-					insert(root->left,data);
-				}
-				else
-					{
-						node *temp = new node;
-					temp->data=data;
-					temp->left=NULL;
-					temp->right=NULL;
-					}
-				}
-			else if(data > root->data)
-            {
-				if(root->right != NULL)
-                {
-					insert(root->right,data);
-				}
-				else
-					{
-						node *temp = new node;
-					    temp->data=data;
-					    temp->left=NULL;
-					    temp->right=NULL;
-					}
+				insert(root->left, data);
 			}
+			else
+			{
+				node *temp = new node;
+				temp->data = data;
+				temp->left = NULL;
+				temp->right = NULL;
 			}
-
+		}
+		else if (data > root->data)
+		{
+			if (root->right != NULL)
+			{
+				insert(root->right, data);
+			}
+			else
+			{
+				node *temp = new node;
+				temp->data = data;
+				temp->left = NULL;
+				temp->right = NULL;
+			}
+		}
+	}
 };
