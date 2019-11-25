39ce97fd6ca2cce61e0391e99f548911c678588d
Tarbooza <hassaan.butt9999@outlook.com>
2019-11-25 18:24:32 +0500

    Added code (not executable) for file handling and printing tree

--- main.cpp
+++ main.cpp
@@ -303,3 +303,35 @@ string line;
 	}
 	File.close();
 */
+
+/*
+void print2DUtil(Node *root, int space)  
+{  
+    // Base case  
+    if (root == NULL)  
+        return;  
+  
+    // Increase distance between levels  
+    space += COUNT;  
+  
+    // Process right child first  
+    print2DUtil(root->right, space);  
+  
+    // Print current node after space  
+    // count  
+    cout<<endl;  
+    for (int i = COUNT; i < space; i++)  
+        cout<<" ";  
+    cout<<root->data<<"\n";  
+  
+    // Process left child  
+    print2DUtil(root->left, space);  
+}  
+  
+// Wrapper over print2DUtil()  
+void print2D(Node *root)  
+{  
+    // Pass initial space count as 0  
+    print2DUtil(root, 0);  
+}  
+*/
